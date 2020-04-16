5a02e59365107e3c99c9fdd75b20cdb1b8fbe3cb
Salomon BRYS <salomon@kodein.net>
2020-04-16 17:13:21 +0200

    gettimeofday is deprecated

--- kodein-log/src/allPosixMain/kotlin/org/kodein/log/timeNative.kt
+++ kodein-log/src/allPosixMain/kotlin/org/kodein/log/timeNative.kt
@@ -6,16 +6,17 @@ import platform.posix.*
 @Suppress("EXPERIMENTAL_UNSIGNED_LITERALS")
 actual fun currentTimeStr(): String {
     memScoped {
-        val curTime = alloc<timeval>()
-        gettimeofday(curTime.ptr, null)
-        val ms = curTime.tv_usec / 1000
+        val ts = alloc<timespec>()
+        clock_gettime(CLOCK_REALTIME, ts.ptr)
+
+        @Suppress("RemoveRedundantCallsOfConversionMethods")
+        val ms = ts.tv_nsec.toLong() / 1_000_000L
         val sec = alloc<time_tVar>()
-        sec.value = curTime.tv_sec
+        sec.value = ts.tv_sec
         val info = localtime(sec.ptr)
 
-        val chars = allocArray<ByteVar>(100)
-
-        strftime(chars, 99.convert(), "%Y-%m-%d %H:%M:%S", info)
+        val chars = allocArray<ByteVar>(32)
+        strftime(chars, 31.convert(), "%Y-%m-%d %H:%M:%S", info)
 
         return chars.toKString() + ":" + ms.toString().padStart(3, '0')
     }
