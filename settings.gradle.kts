f57ed27331a8af68d1cdfa085f792ea4c8e74dea
Romain Boisselle <romain.boisselle@proton.me>
2023-11-24 15:02:53 +0100

    fix: Kotlin 1.9.21

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,11 @@
 buildscript {
     repositories {
         mavenLocal()
+        gradlePluginPortal()
         maven(url = "https://raw.githubusercontent.com/kosi-libs/kodein-internal-gradle-plugin/mvn-repo")
     }
     dependencies {
-        classpath("org.kodein.internal.gradle:kodein-internal-gradle-settings:8.4.2")
+        classpath("org.kodein.internal.gradle:kodein-internal-gradle-settings:8.5.0")
     }
 }
 
